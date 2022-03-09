0db17060005683f3c94278232692a5c9e08dcd8c
tweoss <francischua689@gmail.com>
2022-03-08 21:19:03 -0800

    adding capability to for windowless processes

--- lib/components/process.jsx
+++ lib/components/process.jsx
@@ -1,4 +1,5 @@
 import * as Uebersicht from 'uebersicht'
+import * as Output from '../services/output'
 import * as Slider from '../services/slider'
 
 const getName = (app, title) => {
@@ -10,10 +11,36 @@ const getName = (app, title) => {
 const Process = ({ currentWindow }) => {
   const ref = Uebersicht.React.useRef()
 
-  if (!currentWindow) return null
+  const [output, setOutput] = Uebersicht.React.useState()
 
-  const { app, title } = currentWindow
-  const processName = getName(app, title)
+  const getFrontApp = async () => {
+    const process = await Uebersicht.run(`osascript -e 'tell application "System Events" to get name of application processes whose frontmost is true and visible is true'`);
+    setOutput({ process: Output.cleanup(process) })
+  }
+  
+  
+    Uebersicht.React.useEffect(() => {
+      const init = async () => {
+        await getFrontApp()
+      }
+      init()
+    });
+  const processName = () => {
+    if (!currentWindow) {
+      if (!output) {
+        return ""
+      }
+      else {
+        const { process } = output;
+        return process
+      }
+    }
+
+    else {
+      const { app, title } = currentWindow
+      return getName(app, title)
+    }
+  }
 
   const onMouseEnter = () => Slider.start(ref.current, '.spl-process__inner', '.spl-process__name')
   const onMouseLeave = () => Slider.stop(ref.current, '.spl-process__name')
@@ -21,7 +48,7 @@ const Process = ({ currentWindow }) => {
   return (
     <div ref={ref} className="spl-process" onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
       <span className="spl-process__inner">
-        <span className="spl-process__name">{processName}</span>
+        <span className="spl-process__name">{processName()}</span>
       </span>
     </div>
   )
